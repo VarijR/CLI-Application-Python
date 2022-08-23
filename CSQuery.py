1e42e4665aa4c290c30655a3e71d29b09abf99a9
Varij Rughani <99614337+VarijR@users.noreply.github.com>
2022-08-23 11:47:34 -0400

    Update CSQuery.py

--- CSQuery.py
+++ CSQuery.py
@@ -1,7 +1,8 @@
 '''
  --> Name:- Varij Rughani
  --> Creating a CLI application using argparse to get userinput from terminal and 
-     it will show to get data from db and show on terminal & then it creates a histogram if option -p is given to it 
+     it will show to get data from db and show on terminal 
+ --> And then it creates a histogram if option -p is given to it 
 '''
 
 import sqlite3
